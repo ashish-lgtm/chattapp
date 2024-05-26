29b7d17d8cddb585af981587e6489cf18577a652
ashish-lgtm <f20191042@goa.bits-pilani.ac.in>
2024-05-26 20:11:09 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chattapp-q2tm.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
